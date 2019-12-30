fe56c1231edc9380831c5aba2bb1988927519f59
Temitope Akinsoto <takinsoto@gmail.com>
2019-12-30 21:32:46 +0100

    create a basic app scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text('Hello, world')));
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Center( child: Text('I AM RICH APP') ),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(child: Image(
+              image: NetworkImage('https://images.unsplash.com/flagged/photo-1551592398-c320012bc1c6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')
+          ),),
+        ),
 
+      ),
+    );
