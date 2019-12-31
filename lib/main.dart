9398259be82738fff63c13d32beb9f44156b0577
Temitope Akinsoto <takinsoto@gmail.com>
2019-12-31 15:36:16 +0100

    Add app icon and other image assets to application

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          backgroundColor: Colors.blueGrey,
+          backgroundColor: Colors.blueGrey[900],
           appBar: AppBar(
-            title: Center( child: Text('I AM RICH APP') ),
-            backgroundColor: Colors.blueGrey[900],
+            title: Center(child: Text('I Am Rich App2'),),
+            backgroundColor: Colors.blueGrey,
           ),
           body: Center(child: Image(
-              image: NetworkImage('https://images.unsplash.com/flagged/photo-1551592398-c320012bc1c6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')
+            image: AssetImage('images/diamond.png'),
           ),),
         ),
-
       ),
     );
